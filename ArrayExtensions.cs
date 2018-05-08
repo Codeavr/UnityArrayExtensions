0bf0675c2db66832de95dab25ec5a993066c5c20
Sergey Ankinovich <codeavr@ya.ru>
2018-05-08 21:22:44 +0300

    Removed namespace

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -2,50 +2,53 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Codeavr
+internal static partial class ArrayExtensions
 {
-    internal static partial class ArrayExtensions
+    public static bool InRange<T>(this T[] array, int index)
     {
-        public static bool InRange<T>(this T[] array, int index)
+        return array != null && index >= 0 && index < array.Length;
+    }
+
+    public static bool InRange(this ICollection array, int index)
+    {
+        return array != null && index >= 0 && index < array.Count;
+    }
+
+    public static T PickRandom<T>(this T[] array)
+    {
+        if (array != null && array.Length > 0)
         {
-            return array != null && index >= 0 && index < array.Length;
+            int randomIndex = UnityEngine.Random.Range(0, array.Length);
+            return array[randomIndex];
         }
-        public static bool InRange(this ICollection array, int index)
+        return default(T);
+    }
+
+    public static T PickRandom<T>(this List<T> list)
+    {
+        if (list != null && list.Count > 0)
         {
-            return array != null && index >= 0 && index < array.Count;
+            int randomIndex = UnityEngine.Random.Range(0, list.Count);
+            return list[randomIndex];
         }
-        public static T PickRandom<T>(this T[] array)
-        {
-            if (array != null && array.Length > 0)
-            {
-                int randomIndex = UnityEngine.Random.Range(0, array.Length);
-                return array[randomIndex];
-            }
-            return default(T);
-        }
-        public static T PickRandom<T>(this List<T> list)
-        {
-            if (list != null && list.Count > 0)
-            {
-                int randomIndex = UnityEngine.Random.Range(0, list.Count);
-                return list[randomIndex];
-            }
-            return default(T);
-        }
-        public static void RemoveNulls<T>(this List<T> array)
-        {
-            for (int i = 0; i < array.Count; i++)
-                if (array[i] == null) array.RemoveAt(i--);
-        }
-        public static bool Empty<T>(this T[] obj)
-        {
-            return obj == null || obj.Length == 0;
-        }
-        public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
-        {
-            TValue result;
-            dict.TryGetValue(key, out result);
-            return result;
-        }
-    };
-};
+        return default(T);
+    }
+
+    public static void RemoveNulls<T>(this List<T> array)
+    {
+        for (int i = 0; i < array.Count; i++)
+            if (array[i] == null) array.RemoveAt(i--);
+    }
+
+    public static bool Empty<T>(this T[] obj)
+    {
+        return obj == null || obj.Length == 0;
+    }
+
+    public static TValue Get<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key)
+    {
+        TValue result;
+        dict.TryGetValue(key, out result);
+        return result;
+    }
+}
